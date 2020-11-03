2bcaedd778a1776ead8f63135d31961c0536a4f6
JRGCast <joao.ralph1@gmail.com>
2020-11-03 15:35:25 -0300

    feitos 'esquentas', com exceçao do 6º

---
+++ Fundamentos do Desenvolvimento Web/bloco_5/5.2/JS/conteudo.js
@@ -0,0 +1,23 @@
+// 1. Acesse o elemento elementoOndeVoceEsta.
+document.querySelector("#elementoOndeVoceEsta");
+
+// 2. Acesse pai a partir de elementoOndeVoceEsta e adicione uma color a ele.
+document.querySelector("#elementoOndeVoceEsta").parentNode.style.color = "red";
+
+// 3. Acesse o primeiroFilhoDoFilho e adicione um texto a ele. Você se lembra dos vídeos da aula anterior, como fazer isso?
+document.querySelector("#primeiroFilhoDoFilho").innerHTML = "Olá mundo";
+
+// 4. Acesse o primeiroFilho a partir de pai.
+document.querySelector("#pai").childNodes[1]; // Atenção=> childNodes retorna um array;
+
+// 5. Agora acesse o primeiroFilho a partir de elementoOndeVoceEsta.
+document.querySelector("#elementoOndeVoceEsta").parentNode.firstElementChild; // Primeiro subimos ao pai para então pegar o ELEMENTO filho, pois, o filho[firstChild] é o texto.
+
+// 6. Agora acesse o texto Atenção! a partir de elementoOndeVoceEsta.
+document.querySelector("#elementoOndeVoceEsta") //???????
+
+// 7. Agora acesse o terceiroFilho a partir de elementoOndeVoceEsta.
+.document.querySelector("#elementoOndeVoceEsta").parentNode.childNodes[5];
+
+// 8. Agora acesse o terceiroFilho a partir de pai.
+document.querySelector("#pai").childNodes[5];
