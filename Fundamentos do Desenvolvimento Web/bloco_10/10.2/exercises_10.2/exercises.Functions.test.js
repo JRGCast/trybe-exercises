189a59c65b0244cb56c9f6cd0003b28f3bc2ff7d
JRGCast <joao.ralph1@gmail.com>
2021-01-07 17:58:45 -0300

    wow

--- Fundamentos do Desenvolvimento Web/bloco_10/10.2/exercises_10.2/exercises.Functions.test.js
+++ Fundamentos do Desenvolvimento Web/bloco_10/10.2/exercises_10.2/exercises.Functions.test.js
@@ -1,14 +1,16 @@
+const { doesNotMatch } = require('assert');
+const expectExport = require('expect');
 const { describe } = require('yargs');
 const exercises = require('./exercisesFunctions');
 
 const uppercase = exercises.uppercase;
 
-// describe(`tests function 'uppercase'`),
-//   () => {
-it(`uppercase 'xablau' to equal 'XABLAU'`, (done) => {
-  uppercase('xablau', (str) => {
-    expect(str).toBe('XABLAU');
-    done();
-  });
-});
-//};
+describe(`tests to function 'uppercase'`),
+  () => {
+    it(`uppercase 'xablau' to equal 'XABLAU'`, (done) => {
+      uppercase('xablau', (str) => {
+        expect(str).toBe('XABLAU');
+        done();
+      });
+    });
+  };
--- Fundamentos do Desenvolvimento Web/bloco_10/10.2/informativoAssinc.spec.js
+++ Fundamentos do Desenvolvimento Web/bloco_10/10.2/informativoAssinc.spec.js
@@ -1,20 +1,14 @@
-// test('Não deveria passar!', (done) => {
-//   // Sem o done dá falso positivo/falso 'tudo Ok'
-//   setTimeout(() => {
-//     expect(10).toBe(5);
-//     console.log('Deveria falhar!');
-//   }, 500);
-// });
-
-const SumNumbers = (a, b, callback) => {
+const sumNumbers = require('./informativoAssinc');
+test('Não deveria passar!', (done) => {
+  // Sem o done dá falso positivo/falso 'tudo Ok'
   setTimeout(() => {
-    const result = a + b;
-    callback(result);
+    expect(10).toBe(5);
+    console.log('Deveria falhar!');
   }, 500);
-};
+});
 
-test('Testando SumNumbers, soma 5 mais 10', (done) => {
-  SumNumbers(5, 10, (result) => {
+test('Testando sumNumbers, soma 5 mais 10', (done) => {
+  sumNumbers(5, 10, (result) => {
     expect(result).toBe(15);
     done();
   });
