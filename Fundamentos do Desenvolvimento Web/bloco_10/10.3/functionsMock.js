783bdfd8ec177ccfea2c2cb8bd082bbb0e3ab060
JRGCast <joao.ralph1@gmail.com>
2021-01-10 15:38:09 -0300

    removidos arquivos do bloco 10 para serem adicionados via branch correta

--- Fundamentos do Desenvolvimento Web/bloco_10/10.3/functionsMock.js
+++
@@ -1,7 +0,0 @@
-// 1. Baseando-se nos códigos acima, defina uma função que retorna um número aleatório entre 1 e 100 . A seguir, defina uma outra função que recebe um parâmetro e checa se o número aleatório é divisível pelo número do parâmetro. Assim:
-
-const randomNumber = () => Math.floor(Math.random() * 100); // Lógica de gerar o número
-
-const isDivisible = (number) => randomNumber() % number === 0;
-
-module.exports = { randomNumber, isDivisible };
