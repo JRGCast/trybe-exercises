84a7788aed79ead2bf9b83cb0d19f6556bdf29c3
JRGCast <joao.ralph1@gmail.com>
2021-05-28 16:18:28 -0300

    dando erro 404 no method

--- Desenvolvimento Back-End/bloco_27/27.3/hello-http-methods/index.js
+++ Desenvolvimento Back-End/bloco_27/27.3/hello-http-methods/index.js
@@ -2,15 +2,25 @@
 
 const fetch = require('node-fetch');
 
+
+// Armazenamos o token numa variável.
+// Num ambiente real, esse valor viria do Local Storage, ou de uma variável de ambiente
+const API_TOKEN = '2d635ea9b637ea0f27d58985cc161d64';
+
+// Criamos um novo objeto de Headers
+const headers = new fetch.Headers({
+  Authorization: API_TOKEN});
+
 // Para aquecer, vamos começar com uma requisição do tipo `GET`
-fetch('https://postman-echo.com/get?param1=teste')
+fetch('https://postman-echo.com/get?param1=teste', {
+  headers,
+  method: 'POST',})
   .then((response) => {
     // Ao receber a resposta, verificamos se correu tudo bem
     if (!response.ok) {
       // Caso não, forçamos a Promise a ser rejeitada
       return Promise.reject(response);
     }
-
     // Caso esteja tudo OK, lemos o body como JSON
     return response.json();
   })
@@ -24,7 +34,6 @@ fetch('https://postman-echo.com/get?param1=teste')
     if (errorOrResponse.status) {
       return console.error(`Request failed with status ${errorOrResponse.status}`);
     }
-
     // Caso tenha acontecido um erro de rede (não foi possível completar a request)
     // logamos o erro todo
     console.error(errorOrResponse);
