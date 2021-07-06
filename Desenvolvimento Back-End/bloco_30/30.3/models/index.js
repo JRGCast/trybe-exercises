27618630e6a8c21604ae3498385d753289d04477
JRGCast <joao.ralph1@gmail.com>
2021-07-06 08:57:37 -0300

    30.3ok

--- Desenvolvimento Back-End/bloco_30/30.3/models/index.js
+++ Desenvolvimento Back-End/bloco_30/30.3/models/index.js
@@ -2,10 +2,11 @@
 
 const fs = require('fs');
 const path = require('path');
+console.log(path.resolve('config/config.json'));
 const Sequelize = require('sequelize');
 const basename = path.basename(__filename);
 const env = process.env.NODE_ENV || 'development';
-const config = require(__dirname + '/../config/config.json')[env];
+const config = require(path.resolve('config/config.js'))[env];
 const db = {};
 
 let sequelize;
