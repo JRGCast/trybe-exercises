5577c3e5d643b99c489755c95f7b563327d73ebf
JRGCast <joao.ralph1@gmail.com>
2021-07-06 16:17:26 -0300

    deletada a merda toda, dps precisa ir adicionando submodulos

--- Desenvolvimento Back-End/bloco_26/26.1/hello-world/index.js
+++
@@ -1,6 +0,0 @@
-const readline = require('readline-sync');
-
-const name = readline.question('Qual é o seu nome? ');
-const age = readline.questionInt('Qual é sua idade? ');
-
-console.log(`Hello, ${name}! Welcome to this app, you are ${age} years old heh?`);
