73315730680d7f7a99e26ac4f258fa2a99028ce6
JRGCast <joao.ralph1@gmail.com>
2021-08-17 20:11:08 -0300

    mexido readfiles so para testes do projeto

--- Ciência da Computação/bloco_33/33.2/fixation/readFiles.py
+++ Ciência da Computação/bloco_33/33.2/fixation/readFiles.py
@@ -1,15 +1,43 @@
 import csv
 path = 'jobs.csv'
-results = []
 
+def read(path):
+    """Reads a file from a given path and returns its contents
+    Parameters
+    ----------
+    path : str
+        Full path to file
+    Returns
+    -------
+    list
+        List of rows as dicts
+    """
+    result = []
+    with open(path, 'r') as csvfile:
+        reader = csv.DictReader(csvfile, delimiter=",", quotechar='"')
+        for row in reader:
+            result.append(row)
+    return result
 
-with open(path, 'r') as file:
-        reader = csv.DictReader(file)
-        header, *data = reader
-        line_count = 0
-        for row in data:
-          if row["industry"] not in results and row["industry"] not in (None, ""): 
-            results.append(row["industry"])
-          line_count += 1
-    # print(f"Unique industries: finished analyzing {line_count} rows")
-print(results[-5])
+def get_max_salary(path):
+    """Get the maximum salary of all jobs
+
+    Must call `read`
+
+    Parameters
+    ----------
+    path : str
+        Must be passed to `read`
+
+    Returns
+    -------
+    int
+        The maximum salary paid out of all job opportunities
+    """
+    reader = read(path)
+    # max_salary_list = [ 
+    #   int(row["max_salary"]) for row in reader if (row["max_salary"] not in (None, ""))
+    #         ]
+    print(reader[5])
+
+get_max_salary(path)
