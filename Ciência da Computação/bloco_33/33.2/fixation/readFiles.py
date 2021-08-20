3463b1d8233fc3ccace45646fa8b36582e9e6020
JRGCast <joao.ralph1@gmail.com>
2021-08-20 14:53:17 -0300

    ok

--- Ciência da Computação/bloco_33/33.2/fixation/readFiles.py
+++ Ciência da Computação/bloco_33/33.2/fixation/readFiles.py
@@ -1,43 +1,9 @@
-import csv
-path = 'jobs.csv'
+car = {
+  "brand": "Ford",
+  "model": "Mustang",
+  "year": 1964
+}
 
-def read(path):
-    """Reads a file from a given path and returns its contents
-    Parameters
-    ----------
-    path : str
-        Full path to file
-    Returns
-    -------
-    list
-        List of rows as dicts
-    """
-    result = []
-    with open(path, 'r') as csvfile:
-        reader = csv.DictReader(csvfile, delimiter=",", quotechar='"')
-        for row in reader:
-            result.append(row)
-    return result
+x = car.keys()
 
-def get_max_salary(path):
-    """Get the maximum salary of all jobs
-
-    Must call `read`
-
-    Parameters
-    ----------
-    path : str
-        Must be passed to `read`
-
-    Returns
-    -------
-    int
-        The maximum salary paid out of all job opportunities
-    """
-    reader = read(path)
-    # max_salary_list = [ 
-    #   int(row["max_salary"]) for row in reader if (row["max_salary"] not in (None, ""))
-    #         ]
-    print(reader[5])
-
-get_max_salary(path)
+print(x[0])
